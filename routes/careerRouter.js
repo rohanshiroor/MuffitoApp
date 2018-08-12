cd7763c29bfe323fe01274a15d83d9151413ba4d
Devesh Singh <D3v3sh5ingh@users.noreply.github.com>
2018-08-12 23:27:57 +0530

    Career page added

---
+++ routes/careerRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const privacyRouter = express.Router();
+const path = require('path'); 
+// const validator = require('express-validator');
+// const firebaseui = require('firebaseui');
+privacyRouter.use(bodyParser.json());
+// indexRouter.use(validator());
+privacyRouter.get('/',function(req,res){
+    res.set('Cache-Control','public,max-age=300,s-maxage=600');
+    //res.send('Correcto');
+    res.sendFile(path.join(__dirname,'../views/careers.html'));
+    //res.render('register');
+});
+
+module.exports = privacyRouter;
